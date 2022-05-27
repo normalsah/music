53763b1dd6611099eb6fbdc4ecf4519e4ae6a26c
normalsah <68619310+normalsah@users.noreply.github.com>
2022-05-27 16:02:02 -0400

    Initial upload

---
+++ js.js
@@ -0,0 +1,27 @@
+// Url Constants
+var urlLyric = "https://www.azlyrics.com";
+var urlLyricSearch = "https://search.azlyrics.com/search.php?q=";
+var urlChord = "https://www.ultimate-guitar.com";
+var urlChordSearch = "https://www.ultimate-guitar.com/search.php?search_type=title&value=";
+var urlYoutube = "https://www.youtube.com";
+var urlYoutubeSearch = "https://www.youtube.com/results?search_query=";
+
+// Process Chord Search
+function processChordSearch() {
+  window.location = getTxtSearch() === "" ? urlChord : urlChordSearch + getTxtSearch();
+}
+
+// Process Lyric Search
+function processLyricSearch() {
+  window.location = getTxtSearch() === "" ? urlLyric : urlLyricSearch + getTxtSearch();
+}
+
+// Process Youtube Search
+function processYoutubeSearch() {
+  window.location = getTxtSearch() === "" ? urlYoutube : urlYoutubeSearch + getTxtSearch();
+}
+
+// Get text input
+function getTxtSearch() {
+  return document.getElementById("txtSearch").value;
+}
