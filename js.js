8aaf65c207b6b482d1c5817b80336aea8e2cd167
normalsah <68619310+normalsah@users.noreply.github.com>
2025-09-14 15:18:36 -0400

    Bug fix and improvements
    Fixed Lyric Search, which stopped functioning.
    Searches now open in new tab.

--- js.js
+++ js.js
@@ -2,23 +2,40 @@
 var urlChord = "https://www.ultimate-guitar.com";
 var urlChordSearch = "https://www.ultimate-guitar.com/search.php?search_type=title&value=";
 var urlLyric = "https://www.azlyrics.com";
-var urlLyricSearch = "https://search.azlyrics.com/search.php?q=";
+// var urlLyricSearch = "https://search.azlyrics.com/search.php?q=";
+var urlLyricSearch = "https://www.google.com/search?q=site:azlyrics.com+";
 var urlYoutube = "https://www.youtube.com";
 var urlYoutubeSearch = "https://www.youtube.com/results?search_query=";
 
 // Process Chord Search
 function processChordSearch() {
-  window.location = getTxtSearch() === "" ? urlChord : urlChordSearch + getTxtSearch();
+  var query = getTxtSearch();
+  var url = query === "" ? urlChord : urlChordSearch + encodeURIComponent(query);
+
+  window.open(url, "_blank");
+
+//  window.location = getTxtSearch() === "" ? urlChord : urlChordSearch + getTxtSearch();
 }
 
 // Process Lyric Search
 function processLyricSearch() {
-  window.location = getTxtSearch() === "" ? urlLyric : urlLyricSearch + getTxtSearch();
+  var query = getTxtSearch();
+  var url = query === "" ? urlLyric : urlLyricSearch + encodeURIComponent(query);
+  
+  window.open(url, "_blank");
 }
+
+//function processLyricSearch() {
+//  window.location = getTxtSearch() === "" ? urlLyric : urlLyricSearch + encodeURIComponent(getTxtSearch());
+//}
 
 // Process Youtube Search
 function processYoutubeSearch() {
-  window.location = getTxtSearch() === "" ? urlYoutube : urlYoutubeSearch + getTxtSearch();
+  var query = getTxtSearch();
+  var url = query === "" ? urlYoutube : urlYoutubeSearch + encodeURIComponent(query);
+  window.open(url, "_blank");
+
+//  window.location = getTxtSearch() === "" ? urlYoutube : urlYoutubeSearch + getTxtSearch();
 }
 
 // Process Clear
